845e030bfe747b3aa6d8117e07460e5661c2fcd5
Stephen Cheng <zintaen@gmail.com>
2024-04-07 23:53:45 +0700

    change base url

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -12,7 +12,7 @@ class AppConstants {
   static const int balanceInputLen = 10;
   static const String webHostedUrl = 'https://wholesalelinker.com';
 
-  static const String baseUrl = 'https://wholesalelinker.com';
+  static const String baseUrl = 'https://admin.wholesalelinker.com';
   static const String categoryUri = '/api/v1/categories';
   static const String bannerUri = '/api/v1/banners';
   static const String storeItemUri = '/api/v1/items/latest';
